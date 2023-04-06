9bbc986ce8e7c0bc7e05bdd2c42f864d48727156
unknown <GrahamE@ywcanwil.org>
2023-04-06 14:54:32 -0500

    Added new exercises

---
+++ FunctionExercises.js
@@ -0,0 +1,55 @@
+// * FUNCTION EXERCISES
+
+// * Syntax & Declaration
+
+// TODO Create an empty function called myEmptyFunction
+
+// TODO Create a function that converts a temperature in Farenheight to Celcius, then prints it out
+// ! The formula is: C = (F - 32) * (5/9)
+function convertFahrenheitToCelcius(tempInFahrenheit) {
+
+}
+
+// TODO Create a function that converts a temperature in Celcius to Fahrenheit, then prints it out
+// ! The formula is: F = (C * (9/5)) + 32
+function convertCelciusToFahrenheit(tempInCelcius) {
+
+}
+
+// TODO Create a function that prints out your name (console.log()) 
+
+
+// * Invoking (Calling) a Function
+
+// TODO Create a variable called result
+// TODO Set result equal to convertFahrenheitToCelcius(tempInFahrenehit)
+// TODO Log it to the console.
+// ? What type is result? Why?
+
+
+// * Return Values
+
+// TODO Given a function, return its parameters
+function testFunction(test) {
+
+}
+
+// * Scope
+
+// TODO Create a function (it doesn't matter else is inside of it) and declare a variable of any kind.
+// TODO Attempt to console.log() that variable.
+// ? What happens? Why do you think that is?
+
+
+// * Function() vs Function
+
+let variable1;
+let variable2;
+
+// TODO Set variable1 to testFunction(test). 
+// TODO Set variable2 to testFunction
+// ? These will have different outputs. What is the difference? Why?
+
+
+
+
