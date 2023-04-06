6485bc6f7f4a91765d129003644a08e5b9360d0c
unknown <GrahamE@ywcanwil.org>
2023-04-06 15:37:19 -0500

    Added new exercises

---
+++ EventExercises.js
@@ -0,0 +1,14 @@
+// * EVENT EXERCISES
+
+function handleMouseOver() {
+    document.getElementById("paragraph").style.color = "Red";
+}
+
+// ! The function above is an event handler that changes the color of text when its hovered over.
+
+// TODO Create a new event handler called handleMouseLeave() that changes the color back to white after the mouse leaves it
+
+
+// TODO Create an event handle called handleClick() that changes the screens background color when clicked
+// To do this, you can use document.body.style.backgroundColor and set it to a color of your choice
+
